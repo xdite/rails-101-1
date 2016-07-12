91382f9d5867e42cec9c970ec28880850824d1fd
xdite <xuite.joke@gmail.com>
2016-07-12 22:37:39 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,8 @@ class Group < ApplicationRecord
   belongs_to :user
   has_many :posts
   validates :title, presence: true
+
+  has_many :group_relationships
+  has_many :participated_groups, :through => :group_relationships, :source => :group
+    
 end
