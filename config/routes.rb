48d944ba156a62211927e4e9a933fa23eb2a105c
xdite <xuite.joke@gmail.com>
2016-07-12 22:52:23 +0800

    Fix syntax

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :groups do
     member do
-      post: join
-      post: quit
+      post :join
+      post :quit
     end
     resources :posts
   end
